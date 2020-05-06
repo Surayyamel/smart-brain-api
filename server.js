01ac920b63dea52dd934ed89a88e0cdba536662b
Surayya <Surayyamel@users.noreply.github.com>
2020-05-06 20:09:40 +0200

    adding port

--- server.js
+++ server.js
@@ -40,7 +40,7 @@ app.put('/image', (req, res) => { image.handleImage(req, res, db) })
 app.post('/imageUrl', (req, res) => { image.handleApiCall(req, res) })
 
 
-app.listen(3000, ()=> {
-	console.log('app is running on port 3000');
+app.listen(process.env.PORT || 3000, ()=> {
+	console.log(`app is running on ${process.env.PORT}`);
 });
 
